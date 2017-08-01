625c98c3ac5eef1a82d5b5552509dcdef1b013d9
Dalina Bivolan <dalinabivolan@gmail.com>
2017-08-01 13:41:56 +0300

    tree view added

--- src/main/java/com/oxygenxml/translation/progress/PreviewDialog.java
+++ src/main/java/com/oxygenxml/translation/progress/PreviewDialog.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Enumeration;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -22,8 +23,12 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTree;
 import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeSelectionModel;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
@@ -57,13 +62,13 @@ public class PreviewDialog extends OKCancelDialog {
    * 
    * @param parentFrame   The parent frame of the dialog.
    * @param title   The title of the dialog.
-   * @param list    The relative paths of all the unpacked files.
+   * @param filePaths    The relative paths of all the unpacked files.
    * @param filesOnDisk   Where to copy the unpacked files.
    * @param translatedFiles  Where to extract the archive. These files will be copied in rootDir.
    */
-  public PreviewDialog(final Frame parentFrame, String title, ArrayList<String> list, final File filesOnDisk, final File translatedFiles) {
+  public PreviewDialog(final Frame parentFrame, String title, ArrayList<String> filePaths, final File filesOnDisk, final File translatedFiles) {
     super(parentFrame, title, false);
-    this.filePaths = list;
+    this.filePaths = filePaths;
     
     JButton cancel = getCancelButton();
     // Delete the translatedFiles directory if the user presses the Cancel button.
@@ -124,7 +129,7 @@ public class PreviewDialog extends OKCancelDialog {
 
 
     DefaultListModel<String> paths = new DefaultListModel<String>();
-    for (String string : list) {
+    for (String string : filePaths) {
       paths.addElement(string);
     }
 
@@ -159,15 +164,79 @@ public class PreviewDialog extends OKCancelDialog {
       }
     });
 
-    JScrollPane pathsScrollPane = new JScrollPane(relativePaths); 
-    pathsScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-    pathsScrollPane.setPreferredSize(new Dimension(500, 200));
+    final JScrollPane scrollPane = new JScrollPane();
+    scrollPane.setViewportView(relativePaths);
+    scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+    scrollPane.setPreferredSize(new Dimension(500, 200));
 
-    JLabel label = new JLabel("Double click on a file to see the diferences.");
-    JPanel panel = new JPanel(new GridBagLayout());
+    JLabel label = new JLabel("Double click on an Oxygen supported FILE to see the diferences.");
+    
+    DefaultMutableTreeNode root = new DefaultMutableTreeNode(filesOnDisk.getName());
+    DefaultTreeModel treeModel = new DefaultTreeModel(root);
+    final JTree tree = new JTree(treeModel);
+    
+    for (String data : filePaths) {
+      buildTreeFromString(treeModel, data);
+    }
+    
+    tree.setEditable(false);
+    tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+    tree.setShowsRootHandles(true);
+    
+    final JPanel panel = new JPanel(new GridBagLayout());
+    
+    final JButton treeButton = new JButton("Switch to Tree View");
+    
+    treeButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        if(treeButton.getText().equals("Switch to List View")){
+          scrollPane.setViewportView(relativePaths);
+          treeButton.setText("Switch to Tree View");
+        }else{
+          treeButton.setText("Switch to List View");
+          scrollPane.setViewportView(tree);
+        }
+        
+      }
+    });
+    
+    tree.addMouseListener(new MouseAdapter() {
+      public void mouseClicked(MouseEvent me) {
+        if (me.getClickCount() == 2) {
+          Object[] selectedPath = tree.getSelectionPath().getPath();
+          int length = selectedPath.length;
+          String relativePath = "";
+          for (int i = 1; i < length-1; i++) {
+            relativePath = relativePath + selectedPath[i].toString();
+          }
+          relativePath = relativePath + "/" + selectedPath[length-1];
+          
+          if (logger.isDebugEnabled()) {
+            logger.debug(tree.getSelectionPath());
+          }
+         
+          URL leftURL = null;
+          try {
+            leftURL = new File(filesOnDisk, relativePath).toURI().toURL();
+          } catch (MalformedURLException e2) {
+            e2.printStackTrace();
+          }
+
+          URL rightURL = null;
+          try {
+            rightURL = new File(translatedFiles, relativePath).toURI().toURL();
+          } catch (MalformedURLException e2) {
+            e2.printStackTrace();
+          }
+          ((StandalonePluginWorkspace)PluginWorkspaceProvider.getPluginWorkspace()).openDiffFilesApplication(leftURL, rightURL);
+        }
+      }
+    });
     
     panel.add(label, new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(1, 1, 1, 1), 1, 1));
-    panel.add(pathsScrollPane, new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 1, 1));
+    panel.add(scrollPane, new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 1, 1));
+    panel.add(treeButton, new GridBagConstraints(0, 2, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(1, 1, 1, 1), 1, 1));
+   
     
     getContentPane().add(panel, BorderLayout.CENTER);
 
@@ -176,4 +245,66 @@ public class PreviewDialog extends OKCancelDialog {
     setResizable(true);
     setVisible(true);
   }
+  /**
+   * Builds a tree from a given forward slash delimited string.
+   * 
+   * @param model The tree model.
+   * @param data The string to build the tree from.
+   */
+  private void buildTreeFromString(final DefaultTreeModel model, final String data) {
+    // Fetch the root node
+    DefaultMutableTreeNode root = (DefaultMutableTreeNode) model.getRoot();
+
+    // Split the string around the delimiter
+    String [] strings = data.split("/");
+
+    // Create a node object to use for traversing down the tree as it 
+    // is being created
+    DefaultMutableTreeNode node = root;
+
+    // Iterate of the string array
+    for (String s: strings) {
+      // Look for the index of a node at the current level that
+      // has a value equal to the current string
+      int index = childIndex(node, s);
+
+      // Index less than 0, this is a new node not currently present on the tree
+      if (index < 0) {
+        // Add the new node
+        DefaultMutableTreeNode newChild = new DefaultMutableTreeNode(s);
+        node.insert(newChild, node.getChildCount());
+        node = newChild;
+      }
+      // Else, existing node, skip to the next string
+      else {
+        node = (DefaultMutableTreeNode) node.getChildAt(index);
+      }
+    }
+  }
+  
+  /**
+   * Returns the index of a child of a given node, provided its string value.
+   * 
+   * @param node The node to search its children.
+   * @param childValue The value of the child to compare with.
+   * @return The index.
+   */
+  private int childIndex(final DefaultMutableTreeNode node, final String childValue) {
+    @SuppressWarnings("unchecked")
+    Enumeration<DefaultMutableTreeNode> children = node.children();
+    DefaultMutableTreeNode child = null;
+    int index = -1;
+
+    while (children.hasMoreElements() && index < 0) {
+      child = children.nextElement();
+
+      if (child.getUserObject() != null && childValue.equals(child.getUserObject())) {
+        index = node.getIndex(child);
+      }
+    }
+
+    return index;
+  }
+
+
 }
